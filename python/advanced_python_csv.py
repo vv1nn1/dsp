1f96f1f3e2fa077f817db1fcf72aed94c3561522
vv1nn1 <kuow89@gmail.com>
2017-09-12 18:47:21 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,10 @@
-PLACE YOUR CODE HERE
+import csv
+
+def write_to_csv(list_of_emails):
+    with open('emails.csv', 'w', newline='') as f:
+        writer = csv.writer(f, delimiter="\n")
+        writer.writerow(["list_of_emails"])
+        for row in list_of_emails:
+            writer.writerow([row])
+        #writer.writerow(r for r in list_of_emails)
+   
