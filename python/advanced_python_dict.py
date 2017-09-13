93e65bbd740268802a2c712bc5d893a428f6124d
vv1nn1 <kuow89@gmail.com>
2017-09-12 19:05:12 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,3 +1,6 @@
+"""
+Q6.
+"""
 import csv
 from collections import defaultdict
 def get_dict():
@@ -15,4 +18,41 @@ def get_dict():
         #answer[(lastname[-1],i)] = row[1:]
         answer[lastname[-1]].append(row[1:])
 		
+    return answer
+
+"""
+Q7.
+"""
+import csv
+
+def get_dict():
+
+    with open('faculty.csv','r') as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+    data.pop(0)
+    answer = {}
+    for row in data:
+        name = row[0].split(" ")     
+        answer[tuple(name)] = row[1:]
+    
+    return answer
+
+"""
+Q8.
+"""
+import csv
+from collections import OrderedDict
+
+def get_dict():
+
+    with open('faculty.csv','r') as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+    data.pop(0)
+    answer = {}
+    for row in data:
+        name = row[0].split(" ")
+        answer[tuple(name)] = row[1:]
+        
+    answer_new = OrderedDict(sorted(answer.items(),  key=lambda t: t[0][-1]))
+    print(dict(list(answer_new.items())[:5]))
     return answer
