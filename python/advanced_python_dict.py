cca47f0da3cc257b472b83d4d0fed073cec99298
vv1nn1 <kuow89@gmail.com>
2017-09-12 18:49:03 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+import csv
+from collections import defaultdict
+def get_dict():
+    with open('faculty.csv','r') as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+    data.pop(0)
+    answer = defaultdict(list)
+    
+    for row in data:    
+        lastname = row[0].split(" ")
+        #print(lastname)
+        #i = 0
+        #while (lastname[-1],i) in answer:
+        #    i+=1
+        #answer[(lastname[-1],i)] = row[1:]
+        answer[lastname[-1]].append(row[1:])
+		
+    return answer
