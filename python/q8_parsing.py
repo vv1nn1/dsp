10124b20505fbdd47e6ffb46ca274f70aa5e4292
vv1nn1 <kuow89@gmail.com>
2017-09-12 17:50:14 -0700

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,37 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+def read_data(filename):
+    """Returns a list of lists representing the rows of the csv file data.
+
+    Arguments: filename is the name of a csv file (as a string)
+    Returns: list of lists of strings, where every line is split into a list of values. 
+        ex: ['Arsenal', 38, 26, 9, 3, 79, 36, 87]
+    """
+    with open(filename,'r') as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+    return data
+
+def get_index_with_min_abs_score_difference(goals):
+    """Returns the index of the team with the smallest difference
+    between 'for' and 'against' goals, in terms of absolute value.
+
+    Arguments: parsed_data is a list of lists of cleaned strings
+    Returns: integer row index
+    """
+    goals.pop(0)
+    goal = [x[5] for x in goals]
+    allow = [x[6] for x in goals]
+    diff = [abs(float(x)-float(y)) for x,y in zip(goal,allow)] 
+    return diff.index(min(diff))
+    
+def get_team(index_value, parsed_data):
+    """Returns the team name at a given index.
+    
+    Arguments: index_value is an integer row index
+               parsed_data is the output of `read_data` above
+               
+    Returns: the team name
+    """
+    teams = [x[0] for x in parsed_data]
+    return teams[index_value]
